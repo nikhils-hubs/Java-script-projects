5630d17b4fbb5d533c6c4115ba51130b15217041
Nikhil singh <nikhilsingh1696@gmail.com>
2025-01-11 17:05:35 +0530

    basic clock
    Basic clock to understand the DOM and tried some css

---
+++ clock.js
@@ -0,0 +1,15 @@
+let clocktiming = document.getElementById("clock");
+function updatetime() {
+    let now = new Date()
+    let hours = now.getHours()
+    let minutes = now.getMinutes()
+    let seconds = now.getSeconds()
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    let amPm = hours >= 12 ? "PM" : "AM"
+
+    const clock = `${hours}:${minutes}:${seconds}:${amPm}`
+    clocktiming.innerHTML = clock
+}
+setInterval(updatetime, 1000);
+updatetime();
